2c4170e5d886270887dbee7ba663e614e416d348
Albert <abendayan@itba.edu.ar>
2025-05-19 23:00:13 -0300

    small fixes

--- pca_analysis.py
+++ pca_analysis.py
@@ -57,16 +57,41 @@ plt.tight_layout()
 plt.savefig('pc1_contributions.png')
 plt.close()
 
+# Create bar plot for PC1 scores by country
+plt.figure(figsize=(15, 8))
+pc1_scores = pd.DataFrame({'Country': countries, 'PC1': X_pca[:, 0]})
+pc1_scores_sorted = pc1_scores.sort_values('PC1', ascending=False)
+plt.bar(range(len(pc1_scores_sorted)), pc1_scores_sorted['PC1'])
+plt.xticks(range(len(pc1_scores_sorted)), pc1_scores_sorted['Country'], rotation=45, ha='right')
+plt.ylabel('Score PC1')
+plt.title('Contribución del PC1 por País')
+plt.grid(True, axis='y')
+plt.tight_layout()
+plt.savefig('pc1_countries.png')
+plt.close()
+
+# Create biplot for PC1 vs PC2
 plt.figure(figsize=(12, 8))
 plt.scatter(pca_df['PC1'], pca_df['PC2'])
 
+# Add country labels
 for i, country in enumerate(countries):
     plt.annotate(country, (pca_df['PC1'][i], pca_df['PC2'][i]))
 
-plt.xlabel(f'PC1 ({explained_variance_ratio[0]:.2%} variance explained)')
-plt.ylabel(f'PC2 ({explained_variance_ratio[1]:.2%} variance explained)')
-plt.title('PCA Biplot of European Countries')
+# Add arrows for each variable
+for i, feature in enumerate(X.columns):
+    plt.arrow(0, 0, pca.components_[0][i]*3, pca.components_[1][i]*3, 
+              color='r', alpha=0.5, head_width=0.1)
+    plt.text(pca.components_[0][i]*3.2, pca.components_[1][i]*3.2, 
+             feature, color='r', ha='center', va='center')
+
+plt.xlabel(f'PC1 ({explained_variance_ratio[0]:.2%} varianza explicada)')
+plt.ylabel(f'PC2 ({explained_variance_ratio[1]:.2%} varianza explicada)')
+plt.title('Biplot de Países Europeos')
 plt.grid(True)
+plt.axhline(y=0, color='k', linestyle='-', alpha=0.3)
+plt.axvline(x=0, color='k', linestyle='-', alpha=0.3)
+plt.tight_layout()
 plt.savefig('pca_biplot.png')
 plt.close()
 
